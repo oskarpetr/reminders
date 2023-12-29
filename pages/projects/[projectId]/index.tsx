a42756b19eb153bcc61ba628207c0a7e7fe187e9
Oskar Petr <oskarpetr@macoskar.local>
2023-12-30 00:57:05 +0100

    router fix

--- pages/projects/[projectId]/index.tsx
+++ pages/projects/[projectId]/index.tsx
@@ -9,26 +9,30 @@ import colorToHex from "@/utils/colors";
 import { useParams } from "next/navigation";
 import { Project } from "@/types/Project.types";
 import projects from "@/test-data/projects-data";
+import { useRouter } from "next/router";
 
 export default function Project() {
-  let { projectId } = useParams() ?? { projectId: 1 };
+  let { projectId } = useParams() ?? { projectId: projects[0].id };
+  const routerReady = useRouter().isReady;
 
   const project =
     projects.find((project) => project.id === parseInt(projectId as string)) ??
     projects[0];
 
   return (
-    <Layout project={project}>
-      <Headline
-        text={project.name}
-        icon={<ProjectIcon color={project.color} icon={project.icon} />}
-        color={colorToHex(project.color)}
-        element={<Users users={project.users} />}
-      />
+    routerReady && (
+      <Layout project={project}>
+        <Headline
+          text={project.name}
+          icon={<ProjectIcon color={project.color} icon={project.icon} />}
+          color={colorToHex(project.color)}
+          element={<Users users={project.users} />}
+        />
 
-      {project.tasks.map((task) => {
-        return <Task key={task.taskId} {...task} color={project.color} />;
-      })}
-    </Layout>
+        {project.tasks.map((task) => {
+          return <Task key={task.taskId} {...task} color={project.color} />;
+        })}
+      </Layout>
+    )
   );
 }
