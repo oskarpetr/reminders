27770506e3a2d6ffb3f2945e88635e3ab33242f2
Oskar Petr <oskarpetr@macoskar.local>
2023-12-30 12:54:10 +0100

    seo and title

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,19 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { NextSeo } from "next-seo";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Reminders</title>
+
+        <NextSeo
+          title="Reminders"
+          description="Share and manage your tasks with the reminders app"
+          canonical="reminders.oskarpetr.dev"
+        />
+      </Head>
+
       <body>
         <Main />
         <NextScript />
